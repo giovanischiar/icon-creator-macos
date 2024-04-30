f59df46f57ad822c16ce6481b4fc5bcbc5734c2a
Giovani Schiar <giovanischiar@gmail.com>
2024-04-30 16:55:14 -0700

    Initial Commit, Hello World Project

---
+++ IconCreator/IconCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconCreatorApp.swift
+//  IconCreator
+//
+//  Created by Giovani Schiar on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct IconCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
